83b8996455ad31019d985cc0d834e0561b4648da
thanushree <thanushree.vaishnavi@gmail.com>
2023-09-24 01:50:06 +0530

    first commit

---
+++ bowl.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("btn");
+button.addEventListener("click",play1=>{
+    const music = new Audio("singing-bowl-deep-sound-27532.mp3");
+    music.play();
+})
+// function play1(){
+//     console.log("leeo")
+//     const music = new Audio("singing-bowl-deep-sound-27532.mp3");
+//     console,log(music.play());
+// }
